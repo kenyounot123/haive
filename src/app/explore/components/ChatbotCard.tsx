0b76f93a2092b81624c1df0b692f80015f4203c5
kenyounot123 <kenlu519@gmail.com>
2024-08-06 14:43:09 -0400

    Add reverse prop to ChatbotCard in order to display it differently

--- src/app/explore/components/ChatbotCard.tsx
+++ src/app/explore/components/ChatbotCard.tsx
@@ -1,7 +1,11 @@
 import { Box, Typography } from "@mui/material"
-
+import EastIcon from '@mui/icons-material/East';
+import WestIcon from '@mui/icons-material/West';
 interface HexagonProps {
   chatbotName: string
+}
+interface ChatbotCardProps {
+  reverse: boolean
 }
 const Hexagon = ({chatbotName}: HexagonProps) => {
   return (
@@ -32,13 +36,44 @@ const Hexagon = ({chatbotName}: HexagonProps) => {
     </Box>
   )
 }
-export default function ChatbotCard() {
+export default function ChatbotCard({reverse=false}:ChatbotCardProps) {
   return (
-    <Box sx={{display: "flex"}}>
-      <Hexagon chatbotName={"ChefAI"}/>
-      <Box sx={{p:3, border: '1px solid', borderColor: 'primary.main', borderRadius:3, backgroundColor:"black"}}>
-        <Typography sx={{color: "white", fontSize:24}}>Chatbot for your culinary needs!</Typography>
+    <>
+      {reverse ? (
+        <Box sx={{position:"relative"}}>
+          <Box sx={{position: "absolute", right: 0, top: 0, transform: "translateY(-20px)"}}>
+            <Hexagon chatbotName={"ChefAI"}/>
+          </Box>
+          {/* This is clickable */}
+          <Box sx={{ p:1, border: '1px solid', borderColor: 'primary.main', borderRadius:3, backgroundColor:"black", maxWidth:"80%", mx:'auto', transition: 'background-color 0.3s ease', cursor: 'pointer',
+            '&:hover': {
+              backgroundColor: '#1E1E1E',
+            },
+          }}>
+            <Typography sx={{color: "white", fontSize:24, textWrap:"wrap", maxWidth:"80%", ml:6}}>Chatbot for your culinary needs!</Typography>
+            <Box sx={{display: "flex", justifyContent:"flex-start"}}>
+              <WestIcon sx={{color: "primary.main"}}/>
+            </Box>
+          </Box>
+        </Box>
+      ) : (
+      <Box sx={{position:"relative"}}>
+        <Box sx={{position: "absolute", transform: "translateY(-20px)"}}>
+          <Hexagon chatbotName={"ChefAI"}/>
+        </Box>
+        {/* This is clickable */}
+        <Box sx={{ p:1, border: '1px solid', borderColor: 'primary.main', borderRadius:3, backgroundColor:"black", maxWidth:"80%", mx:'auto',transition: 'background-color 0.3s ease',cursor: 'pointer',
+          '&:hover': {
+            backgroundColor: '#1E1E1E',
+          },
+        }}>
+          <Typography sx={{color: "white", fontSize:24, textWrap:"wrap", maxWidth:"80%", ml:8}}>Chatbot for your culinary needs!</Typography>
+          <Box sx={{display: "flex", justifyContent:"flex-end"}}>
+            <EastIcon sx={{color: "primary.main"}}/>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+      )}
+    </>
   )
 }
--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,12 +1,17 @@
-import { Typography, Box, Container } from "@mui/material";
+import { Typography, Box, Container, Stack } from "@mui/material";
 import HistoryCard from "./components/HistoryCard";
 import ChatbotCard from "./components/ChatbotCard";
 export default function Explore() {
   return (
     <Container sx={{display: "flex", flexDirection:"column", height:"100vh"}}>
       <Box sx={{flexGrow:1}}>
-        <Typography sx={{mb:8, fontWeight: 'bold', color:"white"}} variant="h3">Explore</Typography>
-        <ChatbotCard/>
+        <Typography sx={{mb:8, fontWeight: 'bold', color:"white"}} variant="h4">Explore</Typography>
+
+        <Stack spacing={8}>
+          <ChatbotCard reverse={false}/>
+          <ChatbotCard reverse={true}/>
+          <ChatbotCard reverse={false}/>
+        </Stack>
       </Box>
 
 
@@ -14,7 +19,7 @@ export default function Explore() {
 
       <Box mb={2}>
         <Box sx={{display: "flex", justifyContent:"space-between", alignItems:"center"}}>
-          <Typography sx={{fontWeight: 'bold', color:"white"}} variant="h3">History</Typography>
+          <Typography sx={{fontWeight: 'bold', color:"white"}} variant="h4">History</Typography>
           <Typography sx={{fontWeight: 'light', color:"white", textDecoration: "underline"}}>See all</Typography>
         </Box>
         {/* This should be the user's chat history  */}
