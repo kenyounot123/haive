42b149f77c48b4d0a8a3d6dac294976e2be8a9e6
kenyounot123 <kenlu519@gmail.com>
2024-08-06 16:09:54 -0400

    Add blur effect and login button to history section when user is not logged in

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,9 +1,15 @@
-import { Typography, Box, Container, Stack } from "@mui/material";
+'use client'
+import { Typography, Box, Container, Stack, Button } from "@mui/material";
 import HistoryCard from "./components/HistoryCard";
 import ChatbotCard from "./components/ChatbotCard";
+import Link from "next/link";
+import { useState } from "react";
 export default function Explore() {
+  const [userLoggedIn, setUserLoggedIn] = useState<boolean>(false)
   return (
     <Container sx={{display: "flex", flexDirection:"column", height:"100vh", maxWidth:"900px"}} maxWidth={false}>
+
+
       <Box sx={{flexGrow:1, p:2}}>
         <Typography sx={{mb:8, fontWeight: 'bold', color:"white"}} variant="h4">Explore</Typography>
 
@@ -15,18 +21,51 @@ export default function Explore() {
       </Box>
 
 
-
-
       <Box p={2}>
-        <Box sx={{display: "flex", justifyContent:"space-between", alignItems:"center"}}>
-          <Typography sx={{fontWeight: 'bold', color:"white"}} variant="h4">History</Typography>
-          <Typography sx={{fontWeight: 'light', color:"white", textDecoration: "underline"}}>See all</Typography>
+        <Box sx={{position:"relative"}}>
+          <Box sx={{display: "flex", justifyContent:"space-between", alignItems:"center",filter: userLoggedIn ? 'none' : 'blur(8px)'}}>
+            <Typography sx={{fontWeight: 'bold', color:"white"}} variant="h4">History</Typography>
+            <Link href="/chat">
+              <Typography sx={{fontWeight: 'light', color:"white", textDecoration: "underline"}}>See all</Typography>
+            </Link>
+          </Box>
+          {/* This should be the user's chat history  */}
+          <Box sx={{filter: userLoggedIn ? 'none' : 'blur(5px)'}}>
+            <HistoryCard/>
+            <HistoryCard/>
+            <HistoryCard/>
+          </Box>
+          {!userLoggedIn && (
+            <Box
+              sx={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundColor: "rgb(0,0, 0 / 50%)", // Optional: semi-transparent to indicate blocking
+                zIndex: 99, // Ensure it is on top of other content
+                display: 'flex',
+                alignItems: 'center', // Center vertically
+                justifyContent: 'center',
+              }}
+            > 
+              <Box>
+                <Typography color={"primary.main"} sx={{fontSize:24, mb:2}}>Log In To Access</Typography>
+                <Button sx={{
+                  fontSize: 32, // Increase font size
+                  padding: '12px 32px', // Increase padding for height and width
+                  borderRadius: "12px",
+                }} variant="outlined">
+                  Log In
+                </Button>
+              </Box>
+            </Box>
+          )}
         </Box>
-        {/* This should be the user's chat history  */}
-        <HistoryCard/>
-        <HistoryCard/>
-        <HistoryCard/>
       </Box>
+
+
     </Container>
   )
 }
