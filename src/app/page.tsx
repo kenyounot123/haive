737e7ccc29caf3b7bbefd2f185c0386eb22a1187
bloopgoop <zhu.kevin12@gmail.com>
2024-08-09 14:01:20 -0400

    move landing page code to src

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/app/providers";
-
+import HiveRounded from '@mui/icons-material/Hive';
 
 export default function LandingPage() {
   const router = useRouter();
@@ -13,10 +13,57 @@ export default function LandingPage() {
     signInWithGoogle().then(() => router.push("/explore"));
   }
 
-
   return (
-    <Button onClick={handleSignIn} variant="contained" color="primary">
-      Login
-    </Button>
+    <>
+      <div className="header">
+        <div className="logo">
+          <HiveRounded style={{ fontSize: 40 }} sx={{
+            color: "primary.main"
+          }}/>
+          <div className="logo-text">
+            <header>
+              <h1>
+                h<span className="highlight">AI</span>ve
+              </h1>
+            </header>
+          </div>
+        </div>
+        <div>
+          <a href="#" className="login-button">
+            Login 
+          </a>
+          <button className="menu-button">Menu</button>
+        </div>
+      </div>
+
+      <section className="main">
+        <div>
+          <h1 className="highlight">
+            <span>Unlock the{" "}</span>
+            <span>
+              Ultimate <b className="highlight">AI</b>
+            </span>
+            <br className="line-break"></br>
+            <span>
+              <b className="highlight">Chatbot{" "}</b>
+            </span>
+            <span>Experience</span>
+          </h1>
+          <p style={{
+            color: "white"
+          }}>
+            Discover the ultimate AI chatbot experience with Haive where expert{" "}
+            <br />
+            ChatBots provide instant, specialized insights just for you.
+          </p>
+          <a href="#" className="explore-button">
+            Explore
+          </a>
+        </div>
+      </section>
+      <section>
+        <div className="empty"></div>
+      </section>
+    </>
   );
 }
